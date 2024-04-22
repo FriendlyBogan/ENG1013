8dfdd24450da63e66bab135dd17499066800553e
devnisella <167653989+devnisella@users.noreply.github.com>
2024-04-22 21:13:57 +1000

    Updated from 0.25s to 1s (polled loop)

--- graphing.py
+++ graphing.py
@@ -1,9 +1,9 @@
 import matplotlib as plt
 
 def graphing(polledData):
-    last20SecDistances = polledData[-1:-81]
+    last20SecDistances = polledData[-1:-21]
     last20SecTimes=[]
-    for i in range(0.25,21,0.25):
+    for i in range(1,21,1):
         last20SecTimes.append(i)
 
     plt.plot(last20SecDistances, last20SecTimes)
