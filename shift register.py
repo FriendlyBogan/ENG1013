8730236b340e12ce005ec4dbe3907309d57aefca
csc00p <166264626+csc00p@users.noreply.github.com>
2024-04-24 11:08:53 +1000

    test

--- shift register.py
+++ shift register.py
@@ -6,18 +6,25 @@ MSBFIRST = pymata4.Pymata4().MSBFIRST
 RCLK = 12;  #latchPIN
 SRCLK = 11; #clockPIN
 SER = 14; #data !!DOUBLE CHECK IF THE PINS ARE RIGHT!!
-
+msg = 10000000
 #set the pins to the board as an output
 board.set_pin_mode_digital_output(RCLK)
 board.set_pin_mode_digital_output(SRCLK)
 board.set_pin_mode_digital_output(SER)
 
+#for i in len(msg):
+msg = msg.split()
+digits = msg[i]
+
+
 #make a function with the shift register process
 #make the SR low, to get the data in. connect with the board since it has the pin data
 #def shift_register_process(byte_config):
 board.digital_write(RCLK,0)
-board.digital_write(SER, SRCLK,MSBFIRST,0b10000000) #shift out the byte config
-board.digital_write(RCLK, 1)
+board.digital_write(SER,1)
+board.digital_write(SRCLK,1)
+board.digital_write(RCLK,1)
+
 
 #function to account for all colour light changes
 #def color_light_changes():
