f288f7df7633df8d9bc409ea22b1c30451bf5bd3
nhas0021 <166671577+nhas0021@users.noreply.github.com>
2024-04-25 16:20:28 +0300

    Delete pedpress.py

--- pedpress.py
+++
@@ -1,9 +0,0 @@
-def pedPresence(pin,board,pedcount):
-    board.set_pin_mode_digital_input(pin)
-    try:
-        value = board.digital_read(pin)
-        if value[0] == 0:
-            pedcount += 1
-        return pedcount
-    except KeyboardInterrupt:
-            board.shutdown()
