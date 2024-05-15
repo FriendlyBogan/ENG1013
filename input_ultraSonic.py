7e514a3992fecb8e0cc17c22410fa46bdd1d92aa
Kristian0001 <166669473+Kristian0001@users.noreply.github.com>
2024-05-15 20:35:39 +1000

    added console alert for constant distance

--- input_ultraSonic.py
+++ input_ultraSonic.py
@@ -3,8 +3,13 @@
 # Created Date: 20/04/2024
 # version: 5.0
 
+from pymata4 import pymata4
+import time
 
-def ultraSonic(triggerPin,echoPin,board,ultrasonicData):
+velList = []
+
+def ultrasonic_sensor(triggerPin,echoPin,board,ultrasonicData, distanceTimer):
+
     '''
     Function to read data from ultrasonic sensor
         Parameters:
@@ -12,6 +17,7 @@ def ultraSonic(triggerPin,echoPin,board,ultrasonicData):
             echoPin (int): pin on the arduino connected to the sensor's echo
             board: communication set-up with arduino
             ultrasonicData (list of integers): list of latest values (distances in cm) polled from the sensor
+            distanceTimer (int): counter for counting how many seconds distance is detected for
         Returns:
             Function returns updated ultraSonicData
     '''
@@ -21,16 +27,41 @@ def ultraSonic(triggerPin,echoPin,board,ultrasonicData):
 
     ultrasonicData.append((vals[0])) #only appending the distance
 
-    if (len(ultrasonicData))>2:
+    if len(ultrasonicData) > 2:
         vel = round((ultrasonicData[-2]-ultrasonicData[-1])/(3),2)
         print(f'velocity = {vel}cm/sec')
-        if vel> 10:
+        if vel > 10:
             print('Vehicle speed is over 10cm/s!, Alarms sound')
+
+    if stage == 'StageOne':
+        if len(ultrasonicData) > 2:
+            velList.append(vel)
+            if len(velList) >= 2:
+                if velList[-1] == velList [-2]:
+                    distanceTimer += 1
+                    if distanceTimer > 3:
+                        print('\n == CONSTANT DISTANCE DETECTED ==')
+                else:
+                    distanceTimer = 0
     
     if len(ultrasonicData)==8:    
         ultrasonicData.pop(0)
 
+    if len(velList)==8:
+        velList.pop(0)
+
     print(f'\nData from sensor: {ultrasonicData}')
 
-    return ultrasonicData
+    time.sleep(0.5)
+
+    return ultrasonicData, distanceTimer
   
+def main():
+    distanceTimer = 0
+    board = pymata4.Pymata4()
+    ultrasonicData = []
+    while True:
+        ultrasonicData, distanceTimer = ultrasonic_sensor(triggerPin=5, echoPin=6, board=board, ultrasonicData=ultrasonicData, distanceTimer=distanceTimer)
+
+if __name__ == '__main__':
+    main()
